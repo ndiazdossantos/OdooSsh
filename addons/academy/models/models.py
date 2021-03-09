6bc41769ec2a5fb97aa82be48bda2dda2cd4297d
ndiazdossantos <ndiazdossantos@danielcastelao.org>
2021-03-09 10:14:52 +0100

    añadido a demo

--- addons/academy/models/models.py
+++ addons/academy/models/models.py
@@ -1,3 +1,18 @@
+
+from odoo import models, fields, api
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+
+
+
+
+
+
+
 # -*- coding: utf-8 -*-
 
 # from odoo import models, fields, api
