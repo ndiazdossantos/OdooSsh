6332925073492228a052b6236f8ae24cabe41318
ndiazdossantos <ndiazdossantos@danielcastelao.org>
2021-03-10 10:03:20 +0100

    corrección

--- addons/academy/__manifest__.py
+++ addons/academy/__manifest__.py
@@ -27,22 +27,11 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
-
-    'data': [
-        # 'security/ir.model.access.csv',
-        'templates.xml',
-        'views/openacademy.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-
-    ],
-
-
 
 }
