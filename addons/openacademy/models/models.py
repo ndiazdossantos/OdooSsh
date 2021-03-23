c72fdf73c362e61f569c21dd8b7cf9ede760240b
ndiazdossantos <ndiazdossantos@danielcastelao.org>
2021-03-23 19:50:33 +0100

    modelos Course y Session usando many2one

--- addons/openacademy/models/models.py
+++ addons/openacademy/models/models.py
@@ -7,6 +7,9 @@ class Course(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
 
+    responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
+
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -16,6 +19,11 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course',
+        ondelete='cascade', string="Course", required=True)
+
 
 
 
