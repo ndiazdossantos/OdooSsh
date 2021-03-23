b2394357b159d294606c477e37d64bbc5e06d640
ndiazdossantos <ndiazdossantos@danielcastelao.org>
2021-03-23 20:11:55 +0100

    Relaciones Many2Many múltiples

--- addons/openacademy/models/models.py
+++ addons/openacademy/models/models.py
@@ -25,8 +25,7 @@ class Session(models.Model):
     instructor_id = fields.Many2one('res.partner', string="Instructor")
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
-
-
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
 
 # -*- coding: utf-8 -*-
