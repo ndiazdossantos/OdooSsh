196144c9d9ef8cdca0b3249a69915cd035634daf
ndiazdossantos <ndiazdossantos@danielcastelao.org>
2021-03-23 19:39:20 +0100

    creado modelo Session

--- addons/openacademy/models/models.py
+++ addons/openacademy/models/models.py
@@ -8,8 +8,14 @@ class Course(models.Model):
     description = fields.Text()
 
 
+class Session(models.Model):
+    _name = 'openacademy.session'
+    _description = "OpenAcademy Sessions"
 
-
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
 
 
 
