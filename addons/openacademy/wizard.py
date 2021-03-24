d1221cd91698a3d83aea89eff32a1460e3dc6765
ndiazdossantos <ndiazdossantos@danielcastelao.org>
2021-03-24 12:39:26 +0100

    definida vista formulario y valor predeterminado para campo de wizard

--- addons/openacademy/wizard.py
+++ addons/openacademy/wizard.py
@@ -6,6 +6,10 @@ class Wizard(models.TransientModel):
     _name = 'openacademy.wizard'
     _description = "Wizard: Quick Registration of Attendees to Sessions"
 
+    def _default_session(self):
+        return self.env['openacademy.session'].browse(self._context.get('active_id'))
+
+
     session_id = fields.Many2one('openacademy.session',
-        string="Session", required=True)
+        string="Session", required=True, default=_default_session)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
