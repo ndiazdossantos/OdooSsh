638dfe9b2d86c5cde255a468a2186689cb119aa5
ndiazdossantos <ndiazdossantos@danielcastelao.org>
2021-03-24 13:16:20 +0100

    añadido registro de asistentes a sesiones multiples

--- addons/openacademy/wizard.py
+++ addons/openacademy/wizard.py
@@ -6,15 +6,16 @@ class Wizard(models.TransientModel):
     _name = 'openacademy.wizard'
     _description = "Wizard: Quick Registration of Attendees to Sessions"
 
-    def _default_session(self):
-        return self.env['openacademy.session'].browse(self._context.get('active_id'))
+    def _default_sessions(self):
+        return self.env['openacademy.session'].browse(self._context.get('active_ids'))
 
+    session_ids = fields.Many2many('openacademy.session',
+        string="Sessions", required=True, default=_default_sessions)
 
-    session_id = fields.Many2one('openacademy.session',
-        string="Session", required=True, default=_default_session)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
     def subscribe(self):
-        self.session_id.attendee_ids |= self.attendee_ids
+        for session in self.session_ids:
+            session.attendee_ids |= self.attendee_ids
         return {}
 
